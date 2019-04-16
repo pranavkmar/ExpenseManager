d44e1bb2bdc5b92c730539b1dec8884b66a07043
ISEA <informpranav@gmail.com>
2019-04-16 15:46:32 +0530

    draft

--- app/src/main/java/com/informbytes/expensemanager/MainActivity.java
+++ app/src/main/java/com/informbytes/expensemanager/MainActivity.java
@@ -1,27 +1,29 @@
 package com.informbytes.expensemanager;
 
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+    SQLiteDatabase ExpenseManagerDataBase = null;
+    Button incomeSaveBtn =null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +49,38 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+//        incomeSaveBtn = (Button) findViewById(R.id.incomeSaveBtnView);
+//        incomeSaveBtn.setOnClickListener((View.OnClickListener) getApplicationContext());
+        Button expenseAppendBtn = (Button) findViewById(R.id.expenseAppendBtnView);
+        expenseAppendBtn.setOnClickListener(this);
+        Button retrieveUpdateDbBtn = (Button) findViewById(R.id.retrieveUpdateBtnView);
+        retrieveUpdateDbBtn.setOnClickListener(this);
+
+
+        // Create Database if necessary or open it if exists
+
+        ExpenseManagerDataBase = openOrCreateDatabase("checkbook.db", MODE_PRIVATE, null); // in Mode SQLiteDatabase.CREATE_IF_NECESSARY
+        boolean tableok = false;
+// Check if table exists
+        Cursor c = ExpenseManagerDataBase.query(
+
+                "sqlite_master", null,
+
+                "type=? and name=?",
+
+                new String[]{"table", "checks"},
+
+                null, null, null);
+        if (c.getCount() > 0)
+            tableok = true;
+        if (!tableok) {
+
+            ExpenseManagerDataBase.execSQL("create table ExpenseManagerTable(income real primary key not null,  savedIncome real, item text ,expenditureAmount real, balance real)");
+
+// Income , savedIncome, item, expenditureAmount
+        }
+
     }
 
     @Override
@@ -110,18 +144,22 @@ public class MainActivity extends AppCompatActivity
 
 
     public void AlertDilg(View view) {
+
         callAlert();
+
     }
 
-    public void AlertDilg(MenuItem item) {
+    public void AlertDilg(MenuItem item)  {
         callAlert();
     }
 
     public void callAlert() {
         LayoutInflater inflater = getLayoutInflater();
         View alertLayout = inflater.inflate(R.layout.group_one, null);
-        final EditText etIncome = alertLayout.findViewById(R.id.editText2);
 
+        final EditText etIncome = alertLayout.findViewById(R.id.editText2);
+        incomeSaveBtn =alertLayout.findViewById(R.id.incomeSaveBtnView);
+        incomeSaveBtn.setOnClickListener(this);
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle("SET INCOME in DATABASE");
         // this is set the view from XML inside AlertDialog
@@ -149,4 +187,47 @@ public class MainActivity extends AppCompatActivity
         dialog.show();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.incomeSaveBtnView:
+
+                // TODO: do your code
+                Toast tsave = Toast.makeText(getApplicationContext(), "SAVE BTN CLICKED", Toast.LENGTH_SHORT);
+                tsave.show();
+
+                break;
+
+            case R.id.expenseAppendBtnView:
+                // TODO: do your code
+                Toast.makeText(getApplicationContext(), "Expenses Added in databases", Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case R.id.retrieveUpdateBtnView:
+                // TODO: do your code
+
+                Toast.makeText(getApplicationContext(), "Update Button Pressed", Toast.LENGTH_SHORT).show();
+//                String key = number.getText().toString();
+//
+//                Cursor c = checkbook.query("checks", null, "cheque_number=?",
+//
+//                        new String[]{key}, null, null, null, null);
+//
+//                if (c.getCount() == 0) {
+//
+//                    Toast.makeText(myContext, "no record exists",
+//
+//                            Toast.LENGTH_SHORT).show();
+
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
 }
+
